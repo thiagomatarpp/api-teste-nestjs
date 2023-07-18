912c4aed65d4728163e73c382a37c944740a53eb
Thiago Matar <thiago.matar@picpay.com>
2023-07-18 16:22:22 -0300

    Enabling cors for all origins

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['*']
+  });
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new LogInterceptor());
+  // app.useGlobalInterceptors(new LogInterceptor());
   await app.listen(3000);
 }
 bootstrap();
