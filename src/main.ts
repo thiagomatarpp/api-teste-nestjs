a5251cadbc7a3def282e1381f34f9d8c83e96f40
Thiago Matar <thiago.matar@picpay.com>
2023-07-28 10:16:27 -0300

    Adding swagger stats

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LogInterceptor } from './interceptors/log.interceptor';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import * as swaggerStats from 'swagger-stats';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +13,20 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalInterceptors(new LogInterceptor());
+
+
+  const config = new DocumentBuilder()
+      .setTitle('Cats example')
+      .setDescription('The cats API description')
+      .setVersion('1.0')
+      .addTag('cats')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  app.use(swaggerStats.getMiddleware({ swaggerSpec: document }));
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
